dc4333c864609307917e2252009c2c6d90ef03d3
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-05 22:26:34 +0430

    remaining comments added

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -7,34 +7,36 @@ import (
 )
 
 func AddUint32(x, y uint32) (uint32, bool) {
-	a := uint64(x)
-	b  := uint64(y)
-	if a+b > math.MaxUint32 {
-		return x+y, true
+	a := uint64(x)   // initialize uint64 a with x's value
+	b  := uint64(y)  // initialize uint64 b with y's value
+	if a+b > math.MaxUint32 { // IF a+b is GREATER-THAN MaxUint32, THEN
+		return x+y, true      // return (x+y), and (true)
 	}
-	return x+y, false
+	return x+y, false  // ELSE return (x+y), and (false)
 }
 
 func CeilNumber(f float64) float64 {
-	return math.Ceil(f * 4) / 4
+	return math.Ceil(f * 4) / 4 // formula for Ceiling a number within 0.25
 }
 
 func AlphabetSoup(s string) string {
-	n := strings.Split(s, "")
-	sort.Strings(n)
-	return strings.Join(n, "")
+	n := strings.Split(s, "")  // split
+	sort.Strings(n)                // sort
+	return strings.Join(n, "") // join
 }
 
 func StringMask(s string, n uint) string {
+	// initialized required variables
 	length := len(s)
 	units := int(n)
-	list := strings.Split(s, "")
-	if length == 0 || length == 1{
+	list := strings.Split(s, "") // splits the s string
+	if length == 0 || length == 1{   // length of zero and one returns a single start "*"
 		return "*"
 	}
 	for i := 0; i < length; i++ {
+		// IF i/index of s is GREATER-THAN-OR-EQUAL n/units OR length of s is LESS-THAN-OR-EQUAL to n/units
 		if i >= units || length <= units {
-			list[i] = "*"
+			list[i] = "*" // THEN replace the i/index's string with a single start "*"
 		}
 	}
 	return strings.Join(list, "")
